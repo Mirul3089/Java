6e250a6f3b48f268ac0cbd3626865ac3348de53c
Mirul Patel <55869588+Mirul3089@users.noreply.github.com>
2023-02-22 13:38:59 -0500

    Delete Spring framewrok/J2EE components directory

--- Spring framewrok/J2EE components/Assignment1_Book_Library_api/src/main/java/com/humber/j2ee/Booklibrary.java
+++
@@ -1,9 +0,0 @@
-package com.humber.j2ee;
-
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/api")
-
-public class Booklibrary extends javax.ws.rs.core.Application {
-
-}
--- Spring framewrok/J2EE components/Week5JAXRSwebservice/src/main/java/com/humber/j2ee/LAB4Rest.java
+++
@@ -1,8 +0,0 @@
-package com.humber.j2ee;
-
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class LAB4Rest extends javax.ws.rs.core.Application{
-
-}
